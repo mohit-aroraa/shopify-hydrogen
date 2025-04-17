296c6e3896a65f8318cb8c25b1682a0a8803c36e
Mohit <mohit@anattadesign.com>
2025-04-17 10:16:32 -0500

    custom swiper using metafields

---
+++ app/components/Swiper.jsx
@@ -0,0 +1,67 @@
+import {Swiper, SwiperSlide} from 'swiper/react';
+import {Pagination, Navigation} from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
+import { ChevronRightIcon, ChevronLeftIcon } from '@heroicons/react/24/outline';
+
+export default (props) => {
+  const {slides} = props;
+  return (
+    <Swiper
+      effect="fade"
+      speed={1000}
+      pagination={{
+        clickable: true,
+        el: '.page',
+        bulletActiveClass: 'page-active',
+      }}
+      loop={true}
+      navigation={{
+        nextEl: '.swiper-next',
+        prevEl: '.swiper-prev',
+        
+      }}
+      modules={[Pagination, Navigation]} // Enable loop mode
+      className="w-full h-196 rounded-lg mt-4"
+    >
+      {slides.map((slide) => (
+        <SwiperSlide key={slide.id} className="w-full">
+          <div className="w-full h-196 flex items-center justify-center relative">
+            <img
+              src={slide.media.reference.image.url}
+              alt={slide.title}
+              className="w-full h-196 object-cover absolute inset-0"
+            />
+            <div className="absolute w-auto inset-0 p-8 flex items-center">
+              <div>
+                <h4>{slide.tagline}</h4>
+                <h1 className={`text-[${slide.text_color}] !mb-10 !text-5xl`}>
+                  {slide.title}
+                </h1>
+                <a
+                  href={slide.cta_link}
+                  className={`px-8 py-4 !text-white rounded-full bg-[${slide.text_color}]`}
+                >
+                  {slide.cta_text}
+                </a>
+              </div>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+      <div className="absolute z-10 w-full bottom-0 text-center">
+        <div className='inline-flex mx-auto items-center'>
+        <div className="swiper-prev cursor-pointer">
+            <ChevronLeftIcon className="w-4 h-4" />
+        </div>
+        <div className='page'></div>
+        <div className="swiper-next cursor-pointer">
+            <ChevronRightIcon className="w-4 h-4" />
+        </div>
+        </div>
+      </div>
+    </Swiper>
+  );
+};
--- app/routes/($locale)._index.jsx
+++ app/routes/($locale)._index.jsx
@@ -1,6 +1,7 @@
 import {Await, useLoaderData, Link} from '@remix-run/react';
 import {Suspense} from 'react';
 import {Image, Money} from '@shopify/hydrogen';
+import Swiper from '~/components/Swiper';
 
 /**
  * @type {MetaFunction}
@@ -14,14 +15,43 @@ export const meta = () => {
  */
 export async function loader(args) {
   // Start fetching non-critical data without blocking time to first byte
-  const deferredData = loadDeferredData(args);
+  // const deferredData = loadDeferredData(args);
+
+  const metaObj = await getMetaObjects(args);
+  const metaObjects = metaObj.metaobjects.nodes.map(flat)
 
   // Await the critical data required to render initial state of the page
-  const criticalData = await loadCriticalData(args);
+  // const criticalData = await loadCriticalData(args);
 
-  return {...deferredData, ...criticalData};
+  // console.warn('metaobjects' , metaObjects);
+
+  return {metaObjects}
 }
 
+async function getMetaObjects({context}) {
+  const metaObjects = context.storefront
+  .query(METAOBJECT_QUERY , {
+    variables: {
+      type: 'hero_swiper_content'
+    }
+  })
+  .catch((error) => {
+    // Log query errors, but don't throw them so the page can still render
+    console.error(error);
+    return null;
+  });
+  
+  return metaObjects
+}
+
+const flat = ({ id, handle, media, ...fields }) => ({
+  id,
+  handle,
+  media,
+  ...Object.fromEntries(
+    Object.entries(fields).map(([key, obj]) => [key, obj?.value])
+  ),
+});
 /**
  * Load data necessary for rendering content above the fold. This is the critical data
  * needed to render the page. If it's unavailable, the whole page should 400 or 500 error.
@@ -63,8 +93,7 @@ export default function Homepage() {
   const data = useLoaderData();
   return (
     <div className="home">
-      <FeaturedCollection collection={data.featuredCollection} />
-      <RecommendedProducts products={data.recommendedProducts} />
+      <Swiper slides={data.metaObjects} />
     </div>
   );
 }
@@ -186,6 +215,54 @@ const RECOMMENDED_PRODUCTS_QUERY = `#graphql
     }
   }
 `;
+
+const METAOBJECT_QUERY = `#graphql 
+query GetMetaObjects ($type: String!) {
+metaobjects(first: 10, type: $type) {
+    nodes {
+      id
+      handle
+      media: field(key: "media") {
+      reference {
+        ... on MediaImage {
+          id
+          image {
+            url
+            altText
+            width
+            height
+          }
+        }
+        ... on Video {
+          sources {
+            mimeType
+            url
+          }
+        }
+      }
+      }
+      title: field(key: "title") {
+      value
+      }
+      text_color: field(key: "text_color") {
+      value
+      }
+      cta_color: field(key: "cta_color") {
+      value
+      }
+      cta_url: field(key: "cta_url") {
+      value
+      }
+      cta_text: field(key: "cta_text") {
+      value
+      }
+      tagline: field(key: "tagline") {
+      value
+      }
+      
+    }
+  }
+}`
 
 /** @typedef {import('@shopify/remix-oxygen').LoaderFunctionArgs} LoaderFunctionArgs */
 /** @template T @typedef {import('@remix-run/react').MetaFunction<T>} MetaFunction */
